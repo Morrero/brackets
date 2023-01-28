8bb4f669b8f5884378d6459d79ab34041ff73711
Morrero <111439650+Morrero@users.noreply.github.com>
2023-01-28 05:00:19 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+ 
+  let amount = bracketsConfig.length;
+  let arr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    arr[i] = str[i];
+  }
+
+  function checkMass() {
+    for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < amount; j++) {
+        if (arr[i] == bracketsConfig[j][0]
+          && arr[i + 1] == bracketsConfig[j][1]) {
+          arr.splice(i, 2);
+        }
+      }
+    }
+  }
+
+  let counter = 0;
+
+  do {
+    checkMass();
+    counter++;
+  }
+
+  while (counter < str.length / 2);
+
+  if (arr == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
